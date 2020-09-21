33c4063b8b7fcaae0cd748f16d27f91a121f038e
Phil Deschaine <deschai5@gmail.com>
2020-09-22 06:26:02 +0800

    coming along

--- src/html-to-segments.js
+++ src/html-to-segments.js
@@ -1,26 +1,94 @@
 import { JSDOM } from "jsdom"
+import path from "path"
+import moment from "moment"
 
 export default async function (args = {}) {
-  let { html, parent_selector, top_level_types } = args
-  // sound_effects
+  let {
+    html,
+    selectors,
+    top_level_types,
+    sound_effects,
+    sound_effects_dir
+  } = args
   if (!html?.length) {
     throw new Error("HTML not provided")
   }
   if (!top_level_types?.length) {
     throw new Error("Top level types not provided")
   }
-  if (!parent_selector?.length) {
-    throw new Error("Parent selector not provided")
+  if (!selectors) {
+    throw new Error("Selectors not provided")
+  }
+  if (!sound_effects || !sound_effects_dir) {
+    throw new Error("Sound effects or sound effects directory not provided")
   }
 
   html = new JSDOM(html)
 
   top_level_types = top_level_types.map((type) => {
-    return `${parent_selector} > ${type}`
+    return `${selectors?.parent} > ${type}`
   })
 
   const article_elements = html.window.document.querySelectorAll(
     top_level_types.join(",")
   )
-  console.log(article_elements)
+  console.log(article_elements.length)
+
+  let segments = []
+
+  segments.push({
+    filename: path.join(sound_effects_dir, sound_effects?.start),
+    type: "sound_effect",
+    what: "start_of_article"
+  })
+
+  const article_title = html.window.document.querySelector(selectors?.title)
+    .textContent
+
+  segments.push({
+    text: `${article_title}.`,
+    language_code: `en-GB`,
+    pitch: -5,
+    speed: 1,
+    type: "speech",
+    what: "title"
+  })
+
+  const article_by = html.window.document
+    .querySelector(selectors?.by)
+    .textContent.trim()
+    .split("\n")?.[0]
+    .trim()
+
+  segments.push({
+    text: `${article_by}.`,
+    voice_name: `en-GB-Wavenet-B`,
+    language_code: `en-GB`,
+    pitch: -5,
+    speed: 1,
+    type: "speech",
+    what: "by"
+  })
+
+  let article_date = html.window.document
+    .querySelector(selectors?.date)
+    .textContent.trim()
+    .split("\n")?.[1]
+    .trim()
+
+  article_date = moment(article_date, selectors?.date_format).format(
+    "[On] dddd, MMMM Do YYYY[.]"
+  )
+
+  segments.push({
+    text: article_date,
+    voice_name: `en-GB-Wavenet-B`,
+    language_code: `en-GB`,
+    pitch: -5,
+    speed: 1,
+    type: "speech",
+    what: "date"
+  })
+
+  return segments
 }
--- test/html-to-segments.js
+++ test/html-to-segments.js
@@ -3,31 +3,78 @@ import path from "path"
 import { promises as fsPromises } from "fs"
 import htmlToSegments from "../src/html-to-segments"
 
-test("nothing provided", async (t) => {
+test.beforeEach(async (t) => {
+  t.context.pff_html = await fsPromises.readFile(
+    path.resolve("./test/fixtures/002_pff.html"),
+    "utf-8"
+  )
+})
+
+test("html not provided", async (t) => {
   const error = await t.throwsAsync(htmlToSegments())
   t.is(error.message, "HTML not provided")
 })
 
-test("just html provided", async (t) => {
-  const pff_html = await fsPromises.readFile(
-    path.resolve("./test/fixtures/002_pff.html"),
-    "utf-8"
+test("types not provided", async (t) => {
+  const error = await t.throwsAsync(
+    htmlToSegments({ html: t.context.pff_html })
   )
-
-  const error = await t.throwsAsync(htmlToSegments({ html: pff_html }))
 
   t.is(error.message, "Top level types not provided")
 })
 
-test("just html and types provided", async (t) => {
-  const pff_html = await fsPromises.readFile(
-    path.resolve("./test/fixtures/002_pff.html"),
-    "utf-8"
-  )
-
+test("selectors not provided", async (t) => {
   const error = await t.throwsAsync(
-    htmlToSegments({ html: pff_html, top_level_types: ["p"] })
+    htmlToSegments({ html: t.context.pff_html, top_level_types: ["p"] })
   )
 
-  t.is(error.message, "Parent selector not provided")
+  t.is(error.message, "Selectors not provided")
+})
+
+test("sound effects not provided", async (t) => {
+  const error = await t.throwsAsync(
+    htmlToSegments({
+      html: t.context.pff_html,
+      top_level_types: ["p"],
+      selectors: {}
+    })
+  )
+  t.is(error.message, "Sound effects or sound effects directory not provided")
+})
+
+test("standard call", async (t) => {
+  const top_level_types = [
+    "p",
+    "h1",
+    "h2",
+    "h3",
+    "h4",
+    "h5",
+    "h6",
+    "ul>li",
+    "blockquote",
+    "div"
+  ]
+
+  const segments = await htmlToSegments({
+    html: t.context.pff_html,
+    top_level_types,
+    selectors: {
+      parent: ".m-longform-copy",
+      title: ".g-h1",
+      by: ".m-micro-copy",
+      date: ".m-micro-copy",
+      date_format: "MMM D, YYYY"
+    },
+    sound_effects_dir: "~/Dropbox/Audio/Sounds",
+    sound_effects: {
+      start: "moldy-trace-48000.wav",
+      tweet_replacement: "moldy-prove-48000.wav",
+      unknown_replacement: "nappy-begin-48000.wav"
+    }
+  })
+
+  console.log(segments)
+
+  t.pass()
 })
