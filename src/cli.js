e9d377ea1c6aa8f0559e791cd9a86338846084ab
Phil Deschaine <deschai5@gmail.com>
2021-03-30 15:35:11 +0800

    command-line client taking shape

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,53 @@
 #!/usr/bin/env node
-
+import { terminal as term } from "terminal-kit"
+import packageJson from "../package.json"
+import path from "path"
 ;(async function () {
-  console.log("Earthy Player command line")
-  console.log("---work in progress---")
+  term.on("key", function (name) {
+    if (name === "CTRL_C") {
+      process.exit()
+    }
+  })
+  const logo_url = path.join(
+    "__dirname",
+    "..",
+    "public",
+    "img",
+    "earthy-player-simple.jpg"
+  )
+
+  term.bgWhite.black(
+    "                        Earthy Player                       "
+  )
+  term("\n")
+  await term.drawImage(logo_url, {
+    shrink: { width: 200, height: 30 }
+  })
+
+  await term.bgWhite.black(
+    `                       Version ${packageJson.version}                       `
+  )
+  term("\n\n")
+
+  await term.white(`How do you want to procede?`)
+  term("\n")
+  const menu_items = [
+    "Start with a webpage url",
+    "Start with an earthy-player config file",
+    "Verify my API keys are set up correctly",
+    "Quit"
+  ]
+
+  const choice = await term.singleColumnMenu(menu_items, {
+    keyBindings: {
+      UP: "previous",
+      DOWN: "next",
+      k: "previous",
+      j: "next",
+      ENTER: "submit"
+    }
+  }).promise
+
+  console.log(choice)
+  process.exit()
 })()
