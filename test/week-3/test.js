87d915c378b1e639c08c54999fb07c062ed998c7
Phil Deschaine <deschai5@gmail.com>
2021-04-09 09:50:55 +0800

    reorganize test files cont.

---
+++ test/week-3/test.js
@@ -0,0 +1,34 @@
+import test from "ava"
+import path from "path"
+import fs from "fs-extra"
+import htmlToSegments from "src/html-to-segments"
+import pff_vocab from "test/shared/pff-vocab"
+import pff_discard from "test/shared/pff-discard"
+import voices from "test/shared/voices"
+import sound_effects from "test/shared/sound-effects"
+
+test("html to segments", async (t) => {
+  const html = await fs.readFile(
+    path.join(__dirname, "fixtures", "012_pff_early.html"),
+    "utf-8"
+  )
+  const segments = await htmlToSegments({
+    html,
+    first_para: "Every team that has won",
+    second_para: "We’ll again do three picks",
+    voices,
+    sound_effects_dir:
+      "https://clammy-tennis.s3-ap-southeast-1.amazonaws.com/48000/",
+    sound_effects,
+    discard_if_found: pff_discard,
+    vocab: pff_vocab
+  })
+
+  let solution = await fs.readFile(
+    path.join(__dirname, "fixtures", "013_segments.json"),
+    "utf-8"
+  )
+  solution = JSON.parse(solution)
+
+  t.deepEqual(segments, solution)
+})
