a66821641b1c2b57355ad1927a484b8ead8498a7
Phil Deschaine <deschai5@gmail.com>
2021-04-09 15:21:16 +0800

    another intentionally broken test

---
+++ test/probably/test.js
@@ -0,0 +1,24 @@
+import test from "ava"
+import path from "path"
+import fs from "fs-extra"
+import htmlToSegments from "src/html-to-segments"
+
+test("html to segments", async (t) => {
+  const html = await fs.readFile(
+    path.join(__dirname, "fixtures", "031_probably.html"),
+    "utf-8"
+  )
+  const segments = await htmlToSegments({
+    html,
+    first_para: "most perplexing and enduring mysteries",
+    second_para: "The mystery is hardly unique"
+  })
+
+  let solution = await fs.readFile(
+    path.join(__dirname, "fixtures", "segments.json"),
+    "utf-8"
+  )
+  solution = JSON.parse(solution)
+
+  t.deepEqual(solution, segments)
+})
