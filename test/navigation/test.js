ba0d928627fd53d2d90fd7daeada509c6a5c965b
Phil Deschaine <deschai5@gmail.com>
2021-05-02 19:02:13 -0700

    more tests

---
+++ test/navigation/test.js
@@ -0,0 +1,44 @@
+import test from "ava"
+import path from "path"
+
+import fs from "fs-extra"
+import htmlToSegments from "src/html-to-segments"
+
+test("html to segments", async (t) => {
+  const html = await fs.readFile(
+    path.join(__dirname, "fixtures", "source.html"),
+    "utf-8"
+  )
+
+  const segments = await htmlToSegments({
+    html,
+    first_para: "Fifty-eight selected navigational",
+    second_para: "Under optimal conditions",
+    vocab: {
+      "No.": "Number",
+      "App.": "Apparent"
+    }
+  })
+
+  const segments_solution = JSON.parse(
+    await fs.readFile(
+      path.join(__dirname, "fixtures", "segments_solution.json"),
+      "utf-8"
+    )
+  )
+  t.deepEqual(segments_solution, segments)
+  /*
+  await fs.writeFile(
+    path.join(__dirname, "fixtures", "segments_solution.json"),
+    JSON.stringify(segments, null, 2),
+    "utf-8"
+  )
+
+  await fs.writeFile(
+    path.join(__dirname, "fixtures", "segments_solution.json"),
+    JSON.stringify(segments, null, 2),
+    "utf-8"
+  )
+  t.pass()
+  */
+})
---
+++ test/red-bull/test.js
@@ -0,0 +1,26 @@
+import test from "ava"
+import path from "path"
+
+import fs from "fs-extra"
+import htmlToSegments from "src/html-to-segments"
+
+test("html to segments", async (t) => {
+  const html = await fs.readFile(
+    path.join(__dirname, "fixtures", "article.html"),
+    "utf-8"
+  )
+
+  const segments = await htmlToSegments({
+    html,
+    first_para: "cans and the liquid inside them are manufactured",
+    second_para: "They don’t care whether or not a"
+  })
+
+  const segments_solution = JSON.parse(
+    await fs.readFile(
+      path.join(__dirname, "fixtures", "segments_solution.json"),
+      "utf-8"
+    )
+  )
+  t.deepEqual(segments, segments_solution)
+})
