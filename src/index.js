64f06718cb8f8c7704a5a0f065c99270af1370ac
Phil Deschaine <deschai5@gmail.com>
2022-05-31 18:05:53 -0700

    deps and exposing function

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
+import _getMetadata from "./get-metadata.js"
 import _textToSpeech from "./text-to-speech.js"
 import _condenseSegments from "./condense-segments.js"
 import _htmlToSegments from "./html-to-segments.js"
 import _addChapterMetadata from "./add-chapter-metadata.js"
 
+export async function getMetadata(args) {
+  return await _getMetadata(args)
+}
 export async function htmlToSegments(args) {
   return await _htmlToSegments(args)
 }
